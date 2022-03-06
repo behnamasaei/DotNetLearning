22e9770be8402920640a00ce766f115de38c4c22
behnamasaei <behnamasaei@gmail.com>
2022-03-06 21:31:42 +0330

    Abstractions

---
+++ OOP/ObjectOrientedProgramming/Abstraction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramming
+{
+
+    /// Abstraction
+    /// 
+    public abstract class MyClass
+    {
+        public virtual void Message(string text)
+        {
+            Console.WriteLine($"{text} from base class");
+        }
+    }
+
+    public class SocondClass : MyClass
+    {
+        public override void Message(string text)
+        {
+            //base.Message(text);
+            Console.WriteLine($"{text} from base SocondClass");
+        }
+    }
+
+
+    ///  How Use
+    public class Program
+    {
+        public static void Main()
+        {
+            SocondClass socondClass = new SocondClass();
+            socondClass.Message("Some text...");
+        }
+    }
+
+}

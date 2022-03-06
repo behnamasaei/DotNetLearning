a20b2f95b8b30d3baacd0f4bc76a61beed75e5f6
behnamasaei <behnamasaei@gmail.com>
2022-03-06 20:40:43 +0330

    Implement of Arrays

---
+++ CSharpFundamental/Collections/Program.cs
@@ -0,0 +1,36 @@
+﻿
+//// Array
+
+/// Simple array
+/// 
+int [] arrayOne = new int [5];
+arrayOne [0] = 1;
+
+int[] arrayTwo = { 1, 2, 3, 4, 5 };
+int[] arrayThree = new int[] { 1, 2, 3, 4, 5 };
+
+
+
+/// Multidimensional Arrays
+/// 
+int[,] MArrayOne;
+int[,] MArrayTwo = new int[,] { { 1, 2, }, { 2, 3, } };
+
+
+
+///Jagged Arrays
+///
+int[][] JArrayOne = new int[][] { new int[] { 1, 2 }, new int[] { 3, 4 } };
+
+
+
+/// Param Arrays (method)
+/// 
+void Message(params string[] texts)
+{
+    foreach (var text in texts)
+    {
+        Console.WriteLine(text);
+    }
+}
+Message(new string[] { "Hello" , "World!"});

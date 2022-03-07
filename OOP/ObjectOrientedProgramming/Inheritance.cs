9a3dfb66e2b1e34c8c5cb8fdd59df426050d4a9a
behnamasaei <behnamasaei@gmail.com>
2022-03-07 14:03:03 +0330

    Inheritance init

---
+++ OOP/ObjectOrientedProgramming/Inheritance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramming
+{
+    public class FirstClass
+    {
+        protected string Name { get; set; }
+        protected string Description { get; set; }
+
+        protected void PrintMessage()
+        {
+            Console.WriteLine($"Hello {Name}");
+        }
+    }
+
+
+    public class Inheritance : FirstClass
+    {
+        
+    }
+}

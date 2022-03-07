773a7c41a650b7c3272660177b54ff5f00579fc7
behnamasaei <behnamasaei@gmail.com>
2022-03-07 14:07:50 +0330

    Init polymorphism

---
+++ OOP/ObjectOrientedProgramming/Polymorphism.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramming
+{
+    public class Polymorphism
+    {
+        public virtual void DoWork() { }
+        public virtual int WorkProperty
+        {
+            get { return 0; }
+        }
+    }
+
+    public class DerivedClass : Polymorphism
+    {
+        public override void DoWork() { }
+        public override int WorkProperty
+        {
+            get { return 0; }
+        }
+    }
+}

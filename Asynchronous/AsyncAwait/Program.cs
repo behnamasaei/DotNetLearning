10da158e53dee8f1a879af30eca8949077de1b6c
behnamasaei <behnamasaei@gmail.com>
2022-03-08 15:12:28 +0330

    Complete PLINQ and create md file

---
+++ Asynchronous/AsyncAwait/Program.cs
@@ -0,0 +1,54 @@
+﻿
+public static class Program
+{
+    public static void Main()
+    {
+
+        Console.WriteLine("Before run method with Async");
+        using (Message message = new Message())
+        {
+            message.PrintMessageAsync();
+        }
+        Console.WriteLine("After run method");
+        Console.ReadKey();
+    }
+}
+
+public class Message : IDisposable
+{
+    public void Dispose()
+    {
+
+    }
+
+    public async Task PrintMessageAsync()
+    {
+
+        await Task.Run(async () =>
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                // Without await keyword
+                // not waiting for return value from GetAge method
+                // Output:
+                // 0 and Age is System.Threading.Tasks.Task`1[System.Int32]
+                Console.WriteLine($"{i} and Age is { GetAge()}");
+
+
+                // With await keyword
+                // wait for return value from GetAge method
+                // Output:
+                // 0 and Age is 1
+                Console.WriteLine($"{i} and Age is {await GetAge()}");
+            }
+        });
+    }
+
+
+    public async Task<int> GetAge()
+    {
+        Task.Delay(1000).Wait();
+        return 1;
+
+    }
+}
---
+++ Asynchronous/PLINQ/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+var source = Enumerable.Range(1, 10_000_000);
+
+// Opt in to PLINQ with AsParallel.
+Stopwatch st = new Stopwatch();
+st.Start();
+// call MakeSome method...
+var evenNums = from num in source
+               where num % 2 == 0
+               select Math.Pow(num,100);
+
+st.Stop();
+
+
+
+Stopwatch stt = new Stopwatch();
+stt.Start();
+// call MakeSome method...
+var ovenNums = from num in source.AsParallel()
+               where num % 2 == 0
+               select Awati();
+stt.Stop();
+
+
+Console.WriteLine("Without PLINQ:"+st.ElapsedMilliseconds);
+Console.WriteLine("With PLINQ:"+stt.ElapsedMilliseconds);
+
+ static int Awati()
+{
+    Thread.Sleep(50000);
+    return 1;
+}
+
+Console.WriteLine("{0} even numbers out of {1} total",
+                  evenNums.Count(), source.Count());
+// The example displays the following output:
+//       5000 even numbers out of 10000 total
